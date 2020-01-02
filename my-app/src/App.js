40c9c7a0ccc74ef82883164efd2f27ef43b299e4
boricuaa <nuevodianuevavida@gmail.com>
2020-01-01 18:03:27 -0600

    Create collection Preview and also add some style

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,19 +3,14 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 import Homepage from "./pages/homepage/homepage";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
